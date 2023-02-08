66642ff76e05ded808614eda3dd3a2488cc24f08
ferencprem2 <fercsusz0508@gmail.com>
2023-02-08 11:32:28 +0100

    Made some changes and made the main game

---
+++ Path.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth
+{
+    public class Path
+    {
+        private readonly char _item;
+        private bool _isExit, _west, _north, _east, _south;
+        private int[,] _index;
+        public char Item => _item;
+        public bool West => _west;
+        public bool North => _north;
+        public bool East => _east;
+        public bool South => _south;
+        public bool IsExit => _isExit;
+        public int[,] Index => _index;
+        public Path(char item, bool west, bool north, bool east, bool south)
+        {
+            _item = item;
+
+            _west = west;
+            _north = north;
+            _east = east;
+            _south = south;
+        }
+        public Path(char item, int[,] index)
+        {
+            _item = item;
+            _index = index;
+
+            _west = false;
+            _north = false;
+            _east = false;
+            _south = false;
+        }
+        public Path(char item, int[,] index, bool west, bool north, bool east, bool south)
+        {
+            _item = item;
+            _index = index;
+
+            _west = west;
+            _north = north;
+            _east = east;
+            _south = south;
+        }
+        public void SetIndex(int[,] index)
+        {
+            _index = index;
+        }
+        public void SetExit(bool isExit)
+        {
+            _isExit = isExit;
+        }
+        public int[,] GoWest()
+        {
+            if (!_west) throw new ImpossiblePathException();
+            return new int[_index.GetLength(0), _index.GetLength(1) - 1];
+        }
+        public int[,] GoNorth()
+        {
+            if (!_north) throw new ImpossiblePathException();
+            return new int[_index.GetLength(0) - 1, _index.GetLength(1)];
+        }
+        public int[,] GoEast()
+        {
+            if (!_east) throw new ImpossiblePathException();
+            return new int[_index.GetLength(0), _index.GetLength(1) + 1];
+        }
+        public int[,] GoSouth()
+        {
+            if (!_south) throw new ImpossiblePathException();
+            return new int[_index.GetLength(0) + 1, _index.GetLength(1)];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,158 +4,252 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace Labyrinth
 {
-
-
-
     internal class Program
     {
-
+        static Path[,] maze;
+        static List<Path> mazeConponents = new List<Path>()
+        {
+            new Path('.', false, false, false, false),
+            new Path('█', true, true, true, true),
+            new Path('╬', true, true, true, true),
+            new Path('═', true, false, true, false),
+            new Path('╦', true, false, true, true),
+            new Path('╩', true, true, true, false),
+            new Path('║', false, true, false, true),
+            new Path('╣', true, true, false, true),
+            new Path('╠', false, true, true, true),
+            new Path('╗', true, false, false, true),
+            new Path('╝', true, true, false, false),
+            new Path('╚', false, true, true, false),
+            new Path('╔', false, false, true, true)
+        };
+        static string message = String.Empty;
         static void Main(string[] args)
         {
-            int xAxis = 0;
-            int yAxis = 0;
-            
-
-
-
-            string path = "minta.txt";
-            bool leaveGame = false;
-            Console.WriteLine("Press (s) to start the game");
-            Console.WriteLine("Press (d) to choose difficulty");
-            Console.WriteLine("Press (l) to leave the game");
-            do
+            Menu();
+            string mazePath = "minta.txt";
+            LoadMap(mazePath);
+            message = $"'{mazePath}' pálya betöltve";
+            NormalGame();
+        }
+        static void BlindGame()
+        {
+            int[,] index = new int[5, 6];
+            int[,] asd = new int[10, 20];
+            while (true)
             {
-                var userOptions = Console.ReadKey().Key;
-                switch (userOptions)
+                Console.Clear();
+                Console.WriteLine(asd.GetLength(0));
+                Console.WriteLine(asd.GetLength(1));
+                Console.WriteLine(index.GetLength(0));
+                Console.WriteLine(index.GetLength(1));
+                for (int y = 0; y < asd.GetLength(0); y++)
                 {
+                    for (int x = 0; x < asd.GetLength(1); x++)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        if (index.GetLength(0) == y && x == index.GetLength(1))
+                        {
+                            Console.BackgroundColor = ConsoleColor.Green;
+                        }
+                        Console.Write(".");
+                        Console.ResetColor();
+                    }
+                    Console.WriteLine();
+                }
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.W:
+                        index = new int[index.GetLength(0) - 1, index.GetLength(1)];
+                        break;
+                    case ConsoleKey.A:
+                        index = new int[index.GetLength(0), index.GetLength(1) - 1];
+                        break;
                     case ConsoleKey.S:
-                        Console.Clear();
-                        LoadMapFromFile(path);
-                        DisplayMapOnScreen(LoadMapFromFile(path), xAxis, yAxis);
-                        RoomInMap(LoadMapFromFile(path));
-                        LocateExitPoints(LoadMapFromFile(path));
-                        FakeExitChecker(LocateExitPoints(LoadMapFromFile(path)), LoadMapFromFile(path));
+                        index = new int[index.GetLength(0) + 1, index.GetLength(1)];
                         break;
                     case ConsoleKey.D:
+                        index = new int[index.GetLength(0), index.GetLength(1) + 1];
+                        break;
+                }
+            }
+        }
+        static void Menu()
+        {
+            bool isMenu = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine(message);
+                Console.WriteLine();
+                Console.WriteLine("Press (s) to start the game");
+                Console.WriteLine("Press (d) to choose difficulty");
+                Console.WriteLine("Press (l) to leave the game");
+                Console.WriteLine("Press (b) to go back to menu");
+                Console.WriteLine("Press (k) pálya betöltése");
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.S:
+                        isMenu = false;
+                        break;
+                    case ConsoleKey.K:
+                        string mazePath = "minta.txt";
+                        LoadMap(mazePath);
+                        message = $"'{mazePath}' pálya betöltve";
+                        break;
+                    case ConsoleKey.D:
+                        Console.Clear();
+                        Console.WriteLine("Press (F1) for easy difficulty {The whole map is visible}");
+                        Console.WriteLine("Press (F2) for easy difficulty {The map is not visible}");
+                        Console.WriteLine("Press (F3) for easy difficulty {The whole map is visible but you must complete the game under the given time}");
+                        Console.WriteLine("Press (F4) for easy difficulty {The map is not visible and you must complete the game under the given time}");
+                        switch (Console.ReadKey(true).Key)
+                        {
+                            case ConsoleKey.F1:
+                                Console.Clear();
+                                isMenu = false;
+                                break;
+                            case ConsoleKey.F2:
+                                BlindGame();
+                                break;
+                        }
                         break;
                     case ConsoleKey.L:
                         Environment.Exit(0);
                         break;
                 }
-
-
-            } while (!leaveGame);
+            } while (isMenu);
         }
 
-        static char[,] LoadMapFromFile(string path)
+        //Normal game
+        static void NormalGame()
         {
-            string[] data = File.ReadAllLines(path);
-            char[,] map = new char[data.Length, data[0].Length];
-            for (int i = 0; i < data.Length; i++)
+            int[,] index = new int[1, 0];
+            int[,] next = index;
+            string? message = string.Empty;
+            bool canMove = true;
+            bool isRunning = true;
+            maze[5, 8].SetExit(true);
+            while (isRunning)
             {
-                for (int j = 0; j < data[0].Length; j++)
+                try
                 {
-                    map[i, j] = data[i][j];
+                    Path current = maze[index.GetLength(0), index.GetLength(1)];
+                    if (current.IsExit)
+                    {
+                        Console.WriteLine("This is an exit, are you sure you want to end the game? yes(enter)/no(backspace)");
+                        switch (Console.ReadKey().Key)
+                        {
+                            case ConsoleKey.Backspace:
+                                canMove = true;
+                                isRunning = true;
+                                break;
+                            case ConsoleKey.Enter:
+                                message = "Vége a játéknak";
+                                canMove = false;
+                                isRunning = false;
+                                break;
+                        }
+                    }
+                    Display(index, message);
+                    switch (Console.ReadKey(true).Key)
+                    {
+                        case ConsoleKey.W:
+                            next = current.GoNorth();
+                            break;
+                        case ConsoleKey.A:
+                            next = current.GoWest();
+                            break;
+                        case ConsoleKey.S:
+                            next = current.GoSouth();
+                            break;
+                        case ConsoleKey.D:
+                            next = current.GoEast();
+                            break;
+                    }
+                    message = string.Empty;
+                    if (maze[next.GetLength(0), next.GetLength(1)].Item == '.') throw new ImpossiblePathException();
+                    index = next;
+                }
+                catch (ImpossiblePathException)
+                {
+                    message = "Nem bejárható út";
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    message = "Nem bejárható út";
+                }
+                catch (Exception ex)
+                {
+                    message = ex.Message;
                 }
             }
-
-            return map;
         }
-
-        static void DisplayMapOnScreen(char[,] map, int xAxis, int yAxis)
+        //Displays the player on the screen
+        static void Display(int[,] index, string? message)
         {
-            for (int i = 0; i < map.GetLength(0); i++)
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine(message);
+            Console.WriteLine();
+            try
             {
-                for (int j = 0; j < map.GetLength(1); j++)
+                for (int y = 0; y < maze.GetLength(0); y++)
                 {
-                    Console.SetCursorPosition(xAxis + j, yAxis + i);
-                    Console.Write(map[i, j]);
+                    for (int x = 0; x < maze.GetLength(1); x++)
+                    {
+                        if (index.GetLength(0) == y && x == index.GetLength(1))
+                        {
+                            //Player Color
+                            Console.BackgroundColor = ConsoleColor.Green;
+                        }
+                        //Put player on screen
+                        Console.Write(maze[y, x].Item);
+                        Console.ResetColor();
+                    }
                     Console.WriteLine();
                 }
             }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Üres a labirintus");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
-        static int RoomInMap(char[,] map)
+        static void LoadMap(string path)
         {
-            char room = '█';
-            int roomsInMap = 0;
-            for (int i = 0; i < map.GetLength(0); i++)
+            List<string> lines = new List<string>();
+            StreamReader streamReader = new StreamReader(path);
+            string? line;
+            while ((line = streamReader.ReadLine()) != null)
             {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    if (map[i, j] == room)
-                    {
-                        roomsInMap++;
-                    }
-                }
+                lines.Add(line);
             }
 
-
-
-            Console.WriteLine($"The number of rooms on the map is: {roomsInMap}");
-
-            return roomsInMap;
-        }
-
-        static List<string> LocateExitPoints(char[,] map)
-        {
-            
-            
-            char[] leftsideExits = {'╬', '═', '╦', '╩', '╣', '╗', '╝'};
-            char[] rightsideExits = {'╬', '═', '╦', '╩','╠', '╚', '╔' };
-            char[] topsideExits = {'╬','╩', '║', '╣', '╠','╝', '╚'};
-            char[] bottomtsideExits = {'╬','╦','║', '╣', '╠', '╗','╔' };
-            int coordinateIndex = 0;
-            ValueTuple<int, int, int> exitCoordinates = new ValueTuple<int, int, int>();
-            List<string> realExitCoordinates = new List<string>();
-            
-
-
-            for (int i = 0; i < map.GetLength(0); i++)
+            maze = new Path[lines.Count, lines[0].Length];
+            for (int y = 0; y < maze.GetLength(0); y++)
             {
-                for (int j = 0; j < map.GetLength(1); j++)
+                for (int x = 0; x < maze.GetLength(1); x++)
                 {
-                    if (i == 0 && topsideExits.Contains(map[i, j]))
-                    {
-                        exitCoordinates.Item1 = coordinateIndex;
-                        exitCoordinates.Item2 = i;
-                        exitCoordinates.Item3 = j;
-                        coordinateIndex++;
-                        realExitCoordinates.Add($"{i}-{j}");
-
-                    }
-                    if (i == map.GetLength(0)-1 && bottomtsideExits.Contains(map[i, j]))
-                    {
-                        realExitCoordinates.Add($"{i}-{j}");
-                    }
-                    if (j == 0 && leftsideExits.Contains(map[i, j]))
-                    {
-                        realExitCoordinates.Add($"{i}-{j}");
-                    }
-                    if (j == map.GetLength(1)-1 && rightsideExits.Contains(map[i, j]))
-                    {
-                        realExitCoordinates.Add($"{i}-{j}");
-                    }
+                    Path asd = mazeConponents.Where(z => z.Item == lines[y][x]).First();
+                    maze[y, x] = new Path(asd.Item, new int[y, x], asd.West, asd.North, asd.East, asd.South);
                 }
             }
-
-            Console.WriteLine(exitCoordinates);
-
-            return realExitCoordinates;
-
         }
 
-        static void FakeExitChecker(List<string>exits, char[,] map)
-        {
-            ValueTuple<int, int, int> realExitCoordinates = new ValueTuple<int, int, int>();   
-          
-            
-
-
-        }
+        static void FindExits() { }
+        static void EditMap() { }
+        static void SaveMap(string path) { }
+        static void ChooseLanguage() { }
+        static void ChooseDifficulty() { }
     }
-
 }
